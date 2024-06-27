e7dbcd7181594b905851014dcd8749b30ba5478a
wangning <codingning@163.com>
2024-06-27 16:29:20 +0800

    feat(group): add initial gee framework implementation
    Implement basic functionality for the gee framework, including:
    - Context struct with methods for handling HTTP requests.
    - Engine struct with methods for routing and handling HTTP methods.
    - RouterGroup struct for managing groups of routes.
    - Supporting files and modules such as router.go and trie.go for

---
+++ Go/group/gee/context.go
@@ -0,0 +1,84 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	// origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	Params map[string]string
+	// response info
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
+
+/*
+为什么使用指针接收者
+使用指针接收者有几个原因：
+修改接收者的值：指针接收者允许方法修改接收者的值。
+避免拷贝：对于大的结构体，拷贝值的开销可能很大。使用指针接收者避免了这种拷贝。
+一致性：如果某些方法需要指针接收者，那么最好所有方法都使用指针接收者，以保持一致性
+*/
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
