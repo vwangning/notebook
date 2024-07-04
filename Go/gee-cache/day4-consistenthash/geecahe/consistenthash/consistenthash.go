4a17223608feb251dbe920a7c23de0f79abc486e
wangning <codingning@163.com>
2024-07-04 13:10:29 +0800

    feat(cache): implement initial cache system with LRU and consistent hash

---
+++ Go/gee-cache/day4-consistenthash/geecahe/consistenthash/consistenthash.go
@@ -0,0 +1,29 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map constains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int // Sorted
+	hashMap  map[int]string
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
---
+++ Go/gee-cache/day4-consistenthash/go.mod
@@ -0,0 +1,6 @@
+module example
+
+
+require geecahe v0.0.0
+
+replace geecahe => ./geecahe
